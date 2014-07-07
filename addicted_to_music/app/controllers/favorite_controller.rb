3e3d90784acfc9a2c35cc9ef49a6f03fdcf12738
pollseed <cb.so.devope+0001@gmail.com>
2014-07-08 00:37:38 +0900

    controller add

---
+++ addicted_to_music/app/controllers/favorite_controller.rb
@@ -0,0 +1,4 @@
+class FavoriteController < ApplicationController
+  def index
+  end
+end
--- addicted_to_music/config/routes.rb
+++ addicted_to_music/config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'setting/index'
+
+  get 'favorite/index'
+
+  get 'welcome/index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -12,7 +18,9 @@ Rails.application.routes.draw do
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+    resources :welcome
+    resources :favorite
+    resources :setting
 
   # Example resource route with options:
   #   resources :products do
