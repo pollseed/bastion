53190d13feb783f05e21d615844c75c50d462833
pollseed <cb.so.devope+0001@gmail.com>
2014-07-11 09:27:04 +0900

    welcome ページ作成, top add

--- addicted_to_music/config/routes.rb
+++ addicted_to_music/config/routes.rb
@@ -20,9 +20,12 @@ Rails.application.routes.draw do
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
+
     resources :welcome
+    resources :top
     resources :favorite
     resources :setting
+
 
   # Example resource route with options:
   #   resources :products do
