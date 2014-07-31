57328d7471580da6023ce52c7464d894559e30dc
pollseed <cb.so.devope+0001@gmail.com>
2014-08-01 02:53:21 +0900

    scraping add

---
+++ scraping/script/nikkei_averages_web_scraping_past.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/env ruby
+
+#大枠を定義
+#基本的な流れ
+class scraping
+
+  def initialize
+
+  end
+
+  # 株価データを全取得する
+  def scraping
+
+    # finance pageをスクレイピング
+
+    # DBへ保管する
+    save
+
+  end
+
+  # DBに保管する
+  def save
+
+  end
+
+end
